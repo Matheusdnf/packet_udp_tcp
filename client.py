a7a3c7b42bb62675027217afb84e76cb9143a3ff
Matheusdnf <matheusdiniz870@gmail.com>
2025-11-11 16:42:57 -0300

    feat: tcp

--- client.py
+++ client.py
@@ -4,8 +4,10 @@ print("Iniciando a captura de pacotes. Aperte Ctrl+C para interromper.")
 
 # capturando pacotes
 packets = scapy.sniff()
+
+print(f"aq pai {packets}\n\n\n")
  # sumario de todos pacotes capturados
- # packets.summary()
+packets.summary()
  # looping em todos os pacotes capturados
 for packet in packets:
     # verificando um cabeçalho
@@ -13,3 +15,14 @@ for packet in packets:
         # packet.show()
         print(f"Porta de origem: {packet['UDP'].sport}")
         print(f"Porta de destino: {packet['UDP'].dport}")
+        print(f"Comprimento: {packet['UDP'].len}")
+        print(f"Checksum: {packet['UDP'].chksum}")
+
+    elif packet.haslayer("TCP"):
+        print(f"Porta de origem: {packet['TCP'].sport}")
+        print(f"Porta de destino: {packet['TCP'].dport}")
+        print(f"Comprimento: {packet['TCP'].len}")
+        print(f"Checksum: {packet['TCP'].chksum}")
+
+    else:
+        print(packet.haslayer("ICMP"))
